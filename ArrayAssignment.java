662c643108e092eca9e73e2a3b2c94a7c51d5c01
Alperen Karslıoğlu <a.karslioglu@ug.bilkent.edu.tr>
2023-09-20 16:29:17 +0300

    average problem applied

--- ArrayAssignment.java
+++ ArrayAssignment.java
@@ -17,4 +17,22 @@ public class ArrayAssignment{
     return array;
   }
 
+  public static double[] difference_average(int[] array_input) {
+
+    double sum = 0;
+    for (double i : array_input) {
+        sum += i;
+    }
+    double average = sum / array_input.length;
+
+    double[] result_array = new double[array_input.length];
+
+    for (int i = 0; i < result_array.length; i++) {
+        result_array[i] = array_input[i] - average;
+    }
+    return result_array;
+
+}
+  
+
 }
