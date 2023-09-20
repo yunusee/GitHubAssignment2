b2fe708ebe7a961c426c8507fb721fe3ba77b7f2
Başar Çubukçuoğlu <basarcubukcuoglu@gmail.com>
2023-09-20 16:21:32 +0300

    Array Creation method added
    createArray function added

--- ArrayAssignment.java
+++ ArrayAssignment.java
@@ -1,4 +1,20 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
 public class ArrayAssignment{
   public static void main(String[] args) {
+    
   }
+
+  public static int[] createArray(int num){
+    int[] array = new int[num];
+    Random random = new Random();
+
+    for(int i = 0; i < array.length; i++){
+      array[i] = random.nextInt(101);
+    } 
+    return array;
+  }
+
 }
